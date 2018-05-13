24336e0f3c3632eed41daa59753152a8489d5cfc
Dimon7 <savchukdsma9@gmail.com>
2018-05-13 10:08:10 +0300

    pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
-import {Nav, NavController, Platform} from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import {DebtPage} from "../pages/debt/debt";
-
+import {Component} from '@angular/core';
 
 @Component({
   templateUrl: 'app.html',
@@ -11,10 +6,7 @@ import {DebtPage} from "../pages/debt/debt";
 })
 export class MyApp {
 
-  private navCtrl: NavController;
-
   game = [];
-  newGame = [];
 
   zero(elem){
     elem.history = '';
@@ -48,27 +40,9 @@ export class MyApp {
     localStorage.setItem('game', JSON.stringify(this.game) ) ;
   }
 
-  rec(lenGame){
-    let arr = [];
-    let len = lenGame;
 
-    for (let i = 0; i < len; i++) {
-      for(let j=i+1; j<len; j++) {
-        arr.push({name1: this.game[i].name, name2: this.game[j].name});
-      }
-    }
-    console.log(arr);
 
-    this.navCtrl.push( DebtPage, {obj : arr});
-
-  }
-
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-          this.game =  localStorage.getItem('game') != undefined ?  JSON.parse(localStorage.getItem('game')): [];
-          platform.registerBackButtonAction(() => {
-              navigator['app'].exitApp();
-          });
-      });
+  constructor() {
+      this.game =  localStorage.getItem('game') != undefined ?  JSON.parse(localStorage.getItem('game')): [];
   }
 }
